b3543463b0535c0764396cf37220b6a6d1de2dfc
tcheukh5928 <hervetcheukado@webheadtech.com>
2024-02-24 13:12:12 +0100

    IMPROVEMENT: changing the home pic and adding text

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,7 +7,7 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import ICU from "i18next-icu";
 import logo from "../../images/logoMM.jpg";
-import backgroundImage from "../../images/homemeat.jpg";
+import backgroundImage from "../../images/big_child_dirt.jpeg";
 import "./Home.css";
 
 function Home() {
@@ -35,6 +35,23 @@ function Home() {
       </div>
 
       <hr className="line-spacing-bottom"></hr>
+
+      <div>
+        {/* <h1 className="text-center quotes-font">"</h1> */}
+        <p className="text-center text-font-history">
+        We work with with farmers who feed their cows on a purely grass-based diet. 
+        <br/>
+        We believe that this is how they are designed to live and feed, and we take pride in our herds having a natural way of life, roaming the hills and creating family bonds.  
+        <br/>
+        <br/>
+        Healthy grass is a must as it contributes to our cows living totally natural lives. 
+        <br/>
+        We have seen great results feeding our cows a grass-based diet over the years, and it has been instrumental to helping us run our farm in a nature-focused and cost-effective way
+        <br/>
+        <br/>
+        This is why take a lot of pride in the meat we sell to our customers.
+        </p>
+      </div>
 
       <div>
         <p className="home-text">
